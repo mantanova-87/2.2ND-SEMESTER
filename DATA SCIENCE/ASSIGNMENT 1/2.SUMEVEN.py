bc224224128b46a67ad0ec99c5199e566bfc2e92
MANTAVYA  KUMAR <cdt.mantavyakumar@gmail.com>
2026-01-12 15:55:51 +0530

    WORKING ON FIRST ASSIGNMENT GIVEN BY JP SIR TO CHECK THE LEVEL OF CLASS.

---
+++ DATA SCIENCE/ASSIGNMENT 1/2.SUMEVEN.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 12 10:39:46 2026
+
+@author: MANTAVYA KUMAR
+"""
+s=0
+for i in range(1,100):
+    if i%2==0:
+        s+=i
+        
+print ("SUM OF ALL THE EVEN NUMBERS BETWEEN 1 AND 100 IS: ",s)
